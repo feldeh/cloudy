3231a0dd0b8b17742be93d53bf08f96d4b3bc817
fel <felicien.dehertogh@protonmail.com>
2022-10-26 23:32:24 +0200

    fix alignment on all breakpoints

--- src/components/Card/Widgets/TemperatureWidget/LocationTypo.styled.ts
+++ src/components/Card/Widgets/TemperatureWidget/LocationTypo.styled.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled'
 
 const StyledLocationTypo = styled.p`
   margin-top: 40px;
-  font-size: 1.7em;
+  font-size: 1.9em;
   text-transform: uppercase;
   font-weight: 600;
 `
--- src/components/Card/Widgets/WeatherWidget/WeatherTypo.styled.ts
+++ src/components/Card/Widgets/WeatherWidget/WeatherTypo.styled.ts
@@ -4,15 +4,18 @@ const StyledWeatherTypo = styled.p`
   font-size: 35px;
   font-weight: 700;
   writing-mode: vertical-rl;
-  transform: scale(-1) translateX(0.3em);
-  @media (min-width: 810px) {
+  transform: scale(-1) translateX(0.8em);
+  @media (min-width: 550px) {
     font-size: 40px;
     writing-mode: horizontal-tb;
     transform: scale(1);
   }
+  @media (min-width: 810px) {
+    transform: translateY(-8px);
+  }
   @media (min-width: 1200px) {
-    font-size: 50px;
-    transform: translateY(-45px);
+    font-size: 55px;
+    transform: translateY(-40px);
   }
   margin-top: 5px;
 `
